2a6ba531a245b3cae8d875b4c2f4fdc91850a860
GweesinChan <gweesin2000@gmail.com>
2024-08-04 02:01:06 +0800

    feat: support csv file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import fse from 'fs-extra'
+import type { GansuDetailCnaps } from './api/cnaps'
 import { getCnapsList } from './api/cnaps';
 
 (async function iife() {
   const cnapsList = await getCnapsList()
 
   fse.writeJSON('assets/cnaps.json', cnapsList, { spaces: 2 })
+  fse.writeFile('assets/cnaps.csv', toCsv(cnapsList))
 })()
+
+function toCsv(data: GansuDetailCnaps[]) {
+  const headerRow = `${Object.keys(data[0]).join(',')}\n`
+  const bodyRows = data.map(item => Object.values(item).join(',')).join('\n')
+
+  return headerRow + bodyRows
+}
