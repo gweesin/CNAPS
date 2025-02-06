6155b0272808c586b1cd09497ccee77280d0d66d
GweesinChan <gweesin2000@gmail.com>
2025-02-06 16:41:35 +0800

    style: fix import sort

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,9 @@
-import { expect, it } from 'vitest'
 import { createPatch } from 'diff'
-// @ts-expect-error eslint-disable-next-line ts/ban-ts-comment
-import before from './fixtures/before.csv?raw'
+import { expect, it } from 'vitest'
 // @ts-expect-error eslint-disable-next-line ts/ban-ts-comment
 import after from './fixtures/after.csv?raw'
+// @ts-expect-error eslint-disable-next-line ts/ban-ts-comment
+import before from './fixtures/before.csv?raw'
 
 it('expect version diff', async () => {
   const result = createPatch('version.diff', before, after)
