2fcdddbac8f3ef8997eeab644ccd5d31b66e4080
GweesinChan <gweesin2000@gmail.com>
2025-04-18 19:33:57 +0800

    fix: update commit command to use -m flag for message

--- scripts/commit.ts
+++ scripts/commit.ts
@@ -6,5 +6,5 @@ import { checkDiffMessages } from '../src/diff';
   const after = await readFile('./assets/cnaps.csv', 'utf-8')
   const { stdout: before } = await execa(`git`, ['--no-pager', 'show', 'HEAD^:./assets/cnaps.csv'] /* { stdout: 'inherit' } */)
   const lines = checkDiffMessages(before, after)
-  await execa('git', ['commit', `feat: update cnaps data\n\n${lines.join('\n')}`])
+  await execa('git', ['commit', '-m', `feat: update cnaps data\n\n${lines.join('\n')}`])
 })()
