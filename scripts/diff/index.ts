f01673cc04340563a0253a5f79758ac2bb0a018e
GweesinChan <gweesin2000@gmail.com>
2025-04-18 19:04:24 +0800

    build: support cnaps data checking diff

--- scripts/diff/index.ts
+++ scripts/diff/index.ts
@@ -1,28 +1,76 @@
-import fs from 'node:fs'
-import { diffJson } from 'diff'
+import type { GansuDetailCnaps } from '../generate/api/cnaps'
+import { diffLines } from 'diff'
 
-// Read the old and new CNAPS data
-const oldData = fs.existsSync('assets/cnaps.json')
-  ? JSON.parse(fs.readFileSync('assets/cnaps.json', 'utf8'))
-  : []
+export type ChangeType = 'modify' | 'add' | 'remove'
+export interface Changes {
+  changeType: ChangeType
+  bankCode: string
+  oldValue?: GansuDetailCnaps
+  newValue?: GansuDetailCnaps
+}
 
-const newData = JSON.parse(fs.readFileSync('assets/cnaps.json.new', 'utf8'))
+export function checkDiff(oldStr: string, newStr: string) {
+  const lines = diffLines(oldStr, newStr)
 
-// Compare the data using diff
-const differences = diffJson(oldData, newData)
+  const removedMap: Map<string, GansuDetailCnaps> = new Map()
+  const addedMap: Map<string, GansuDetailCnaps> = new Map()
+  const diffs: Changes[] = []
+  lines.forEach((change) => {
+    if (!change.added && !change.removed) {
+      return
+    }
 
-// Print the differences
-differences.forEach((part) => {
-  if (part.added) {
-    console.log('Added:', part.value)
-  }
-  if (part.removed) {
-    console.log('Removed:', part.value)
-  }
-})
+    const [LName, BankCode, BankName, BankId, CityCode, CityName, ProvinceName, ProvinceCode] = change.value.split(',')
+    const data = {
+      LName,
+      BankCode,
+      BankName,
+      BankId,
+      CityCode,
+      CityName,
+      ProvinceName,
+      ProvinceCode,
+    } as GansuDetailCnaps
 
-// Output summary
-console.log('\nSummary:')
-console.log('Old entries:', oldData.length)
-console.log('New entries:', newData.length)
-console.log('Difference:', newData.length - oldData.length)
+    if (change.added) {
+      // confirm modified data if exist in removedMap
+      const removedData = removedMap.get(BankCode)
+      if (removedData) {
+        diffs.push({ changeType: 'modify', bankCode: BankCode, oldValue: removedData, newValue: data })
+        removedMap.delete(BankCode)
+        return
+      }
+
+      addedMap.set(BankCode, data)
+    }
+    else if (change.removed) {
+      removedMap.set(BankCode, data)
+    }
+  })
+
+  addedMap.forEach((data) => {
+    diffs.push({ changeType: 'add', bankCode: data.BankCode, newValue: data })
+  })
+  removedMap.forEach((data) => {
+    diffs.push({ changeType: 'remove', bankCode: data.BankCode, oldValue: data })
+  })
+  return diffs
+}
+
+export function checkDiffMessages(oldStr: string, newStr: string) {
+  const diffs = checkDiff(oldStr, newStr)
+  // eslint-disable-next-line array-callback-return
+  return diffs.map((change) => {
+    if (change.changeType === 'modify') {
+      return `“${change.oldValue?.LName}” 更名为 “${change.newValue?.LName}”`
+    }
+
+    if (change.changeType === 'add') {
+      return `新增 “${change.newValue?.LName}”`
+    }
+
+    if (change.changeType === 'remove') {
+      return `移除 “${change.oldValue?.LName}”`
+    }
+  })
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,33 @@
-import { createPatch } from 'diff'
-import { expect, it } from 'vitest'
-// @ts-expect-error eslint-disable-next-line ts/ban-ts-comment
-import after from './fixtures/after.csv?raw'
-// @ts-expect-error eslint-disable-next-line ts/ban-ts-comment
-import before from './fixtures/before.csv?raw'
+import { describe, expect, it } from 'vitest'
+import { checkDiff } from '../scripts/diff'
 
-it('expect version diff', async () => {
-  const result = createPatch('version.diff', before, after)
-  await expect(result).toMatchFileSnapshot('__snapshots__/version.diff')
+const header = `LName,BankCode,BankName,BankId,CityCode,CityName,ProvinceName,ProvinceCode`
+const lineOne = `中国工商银行股份有限公司杭州钱塘支行,102331005059,中国工商银行,102100099996,3310,杭州市,浙江省,33`
+const lineTwo = `中国工商银行股份有限公司北京金台路支行,102100002020,中国工商银行,102100099996,1000,北京市,北京市,11`
+const lineOneAfter = `中国工商银行杭州经济技术开发区支行,102331005059,中国工商银行,102100099996,3310,杭州市,浙江省,33`
+
+function mergeCsv(...lines: string[]) {
+  return [header, ...lines].join('\n')
+}
+
+describe('checkDiff', () => {
+  it('modify', async () => {
+    const result = checkDiff(mergeCsv(lineOne), mergeCsv(lineOneAfter))
+    await expect(result).toMatchFileSnapshot('__snapshots__/modify.diff')
+  })
+
+  it('add', async () => {
+    const result = checkDiff(mergeCsv(), mergeCsv(lineOne))
+    await expect(result).toMatchFileSnapshot('__snapshots__/add.diff')
+  })
+
+  it('delete', async () => {
+    const result = checkDiff(mergeCsv(lineOne), mergeCsv())
+    await expect(result).toMatchFileSnapshot('__snapshots__/delete.diff')
+  })
+
+  it('hydrate', async () => {
+    const result = checkDiff(mergeCsv(lineOne), mergeCsv(lineOneAfter, lineTwo))
+    await expect(result).toMatchFileSnapshot('__snapshots__/hydrate.diff')
+  })
 })
