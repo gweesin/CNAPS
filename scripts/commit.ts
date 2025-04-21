c7cfc173e43135bae26b35a1234e4e5124b890e3
GweesinChan <gweesin2000@gmail.com>
2025-04-21 21:03:23 +0800

    build: fix commit assets script

--- scripts/commit.ts
+++ scripts/commit.ts
@@ -4,7 +4,8 @@ import { checkDiffMessages } from '../src/diff';
 
 (async function iife() {
   const after = await readFile('./assets/cnaps.csv', 'utf-8')
-  const { stdout: before } = await execa(`git`, ['--no-pager', 'show', 'HEAD^:./assets/cnaps.csv'] /* { stdout: 'inherit' } */)
+  await execa('git', ['add', './assets/*'])
+  const { stdout: before } = await execa('git', ['--no-pager', 'show', 'HEAD^:./assets/cnaps.csv'] /* { stdout: 'inherit' } */)
   const lines = checkDiffMessages(before, after)
   await execa('git', ['commit', '-m', `feat: update cnaps data\n\n${lines.join('\n')}`])
 })()
